dd2d72b659171d93c3ca1d4df9e333f3b73b21bd
Seoeun, Cho <westsilver24@naver.com>
2023-02-27 20:43:49 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,9 +1,7 @@
 function solution(arr) {
-    let answer = 0;
-    let sum = 0;
-    
-    sum = arr.reduce((pre, cur) => pre + cur);
-    answer = sum / arr.length;
-    
-    return answer;
+    return arr.reduce((pre, cur) => pre + cur) / arr.length;
 }
+
+// 테스트 예제
+let arr = [5, 2, 3];
+console.log ('평균값 : ', solution(arr));
