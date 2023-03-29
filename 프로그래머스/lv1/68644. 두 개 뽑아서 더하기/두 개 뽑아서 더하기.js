ce017b2750f9fbd21497a493f608727ea4b19589
Seoeun, Cho <westsilver24@naver.com>
2023-03-30 00:11:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,12 @@
 function solution(numbers) {
     let answer = [];
     
-    for(let i=0; i<numbers.length; i++) {
-        for(let j=i+1; j<numbers.length; j++) {
-            let numSum = numbers[i]+numbers[j];
-            if(!answer.includes(numSum)) answer.push(numSum);
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            let sum = numbers[i] + numbers[j];
+            if (!answer.includes(sum)) answer.push(sum);
         }
     }
     
-    // 배열 오름차순 정렬
-    return answer.sort((a, b) => a-b);
+    return answer.sort((a, b) => a - b);
 }
