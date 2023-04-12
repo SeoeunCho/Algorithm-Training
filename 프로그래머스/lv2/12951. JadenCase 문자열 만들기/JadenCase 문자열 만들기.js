c5c127fc8d59f11c71290ee48fc3d096e75eb40b
Seoeun, Cho <westsilver24@naver.com>
2023-04-12 17:22:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,16 +1,23 @@
+// 정규식 표현을 이용한 풀이
 function solution(s) {
     let answer = '';
     let arr = [];
     
     // 문자열 분리 및 연속되는 공백 제거
-    // s = s.split(' ').filter(x => x !== '');
     s = s.split(' ');
     
     for (let i = 0; i < s.length; i++) {
+        // 공백이 아닌 단어만 찾아 JadenCase로 변환
         if (s[i] !== '') {
-            let firstStr = s[i][0].toUpperCase();
-        
+            
+            // 첫 문자가 대문자가 아닌 것만 찾아 대문자로 변환
+            let firstStr = s[i][0]; // s[i][0].toUpperCase(); 만으로도 대체 가능
+            if (firstStr.match(new RegExp(/^[a-z]/))) {
+                firstStr = firstStr.toUpperCase();
+            }
+            
             let word = firstStr;
+            // 첫 문자 외의 알파벳이 소문자가 아닌 것만 찾아 소문자로 변환
             for (let j = 1; j < s[i].length; j++) {
                 let otherStr = s[i][j];
                 if (otherStr.match(new RegExp(/^[A-Z]/))) {
@@ -18,37 +25,38 @@ function solution(s) {
                 }
                 word += otherStr;
             }
-            // answer = arr.join(' ');
+            
+            // 변환된 단어를 원래 문자열에 대입
             s[i] = word;
-            answer = s.join(' ');
         }
     }
     
-    return answer;
+    return answer = s.join(' ');
 }
 
-// 대소문자를 구분하는 방식
-// 참고링크 : https://developer-talk.tistory.com/789
+/* 대소문자를 구분하는 방식
+참고링크 : https://developer-talk.tistory.com/789 */
 
 
-// function solution(s) {
-//     let answer = '';
-//     s = s.split(' ');
+
+
+
+
+
+// 첫 알파벳 및 이외의 알파벳이 대소문자인지 아닌지 구별없이 푼 풀이
+function solution(s) {
+    let answer = '';
+    s = s.split(' ');
     
-//     for (let i = 0; i < s.length; i++) {
-//         let arr = s[i].split(''); // 각 단어별로 split해서 배열에 담음
-//         let firstStr = arr.shift().toUpperCase(); // 첫번쨰 스트링 구하기
-        
-//         arr.unshift(firstStr);
-        
-//         if (i < s.length - 1) arr.push(' ');
-        
-//         for (let j = 1; j < arr.length; j++) {
-//             arr[j] = arr[j].toLowerCase();
-//         }
-        
-//         answer += arr.join('');
-//     }
-    
-//     return answer;
-// }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== '') {
+            let firstStr = s[i][0].toUpperCase(); // 첫번째 스트링 구하기
+            let result = firstStr;
+            for (let j = 1; j < s[i].length; j++) {
+                result += s[i][j].toLowerCase();
+            }
+            s[i] = result;
+        }
+    }
+    return answer = s.join(' ');
+}
