a9fc31bd9e2097751beb09b6d36885e3c5a94fc5
Seoeun, Cho <westsilver24@naver.com>
2023-04-18 10:29:39 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 12.04 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,19 +1,18 @@
 function solution(s) {
-    let zeroSum = 0;
-    let changeSum = 0;
+    let answer = [0, 0];
     
     while (s > 1) {
         // 0의 개수
-        zeroSum += s.split('').filter(x => x === '0').length;
+        answer[1] += s.split('').filter(x => x === '0').length;
         
         // 0을 빼고 1만 남은 배열의 길이를 이진 변환한 결과
         s = s.split('').filter(x => x !== '0').length.toString(2);
         
         // 변환 횟수
-        changeSum++;
+        answer[0]++;
         
-        console.log(s, zeroSum, changeSum);
+        console.log(s, answer[1], answer[0]);
     }
     
-    return [changeSum, zeroSum];
+    return answer;
 }
