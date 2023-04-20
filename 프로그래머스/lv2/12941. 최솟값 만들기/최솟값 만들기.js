459e5f9234e771686eaf4c3f12fdb72fd24a640b
Seoeun, Cho <westsilver24@naver.com>
2023-04-20 18:11:18 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,20 +1,23 @@
-// function solution(A,B){
-//     let answer = 0;
+// 오답 - 효율성테스트 불합격
+function solution(A,B){
+    let answer = 0;
     
-//     while (A.length > 0) {
-//         let a = Math.min(...A);
-//         A.splice(A.indexOf(a), 1);
+    while (A.length > 0) {
+        let a = Math.min(...A);
+        A.splice(A.indexOf(a), 1);
 
-//         let b = Math.max(...B);
-//         B.splice(B.indexOf(b), 1);
+        let b = Math.max(...B);
+        B.splice(B.indexOf(b), 1);
         
-//         answer += a * b;
-//     }
+        answer += a * b;
+    }
     
-//     return answer;
-// }
+    return answer;
+}
 
 
+
+// 다른 풀이 참고
 function solution(A, B) {
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
