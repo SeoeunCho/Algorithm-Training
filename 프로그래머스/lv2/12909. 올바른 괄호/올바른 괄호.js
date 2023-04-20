70d253c96d4d4e86ef5fb3f18e5c183817cff405
Seoeun, Cho <westsilver24@naver.com>
2023-04-20 16:41:49 +0900

    [level 2] Title: 올바른 괄호, Time: 2.89 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,22 @@
+function solution(s){
+    let answer = true;
+    
+    if (s.charAt(0) === ')' || s.charAt(s.length - 1) === '(') return false;
+    
+    // 연속되는 여는 괄호의 수를 세서 닫는 괄호의 개수가 더 많을 시 false를 반환
+    let cntOpen = 0;
+    for(let i = 0; i < s.length; i++) {
+        s[i] === '(' ? cntOpen++ : cntOpen--;
+        if (cntOpen < 0) return false;
+    }
+     
+    return cntOpen ? false : true;
+}
+    
+    
+    
+    
+    
+    
+    
+    
