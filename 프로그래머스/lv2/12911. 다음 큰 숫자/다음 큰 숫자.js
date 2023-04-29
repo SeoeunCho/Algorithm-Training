288174c67ca45aa43ed3892debed51b50c1ca65c
Seoeun, Cho <westsilver24@naver.com>
2023-04-30 00:42:40 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,13 +1,16 @@
 function solution(n) {
-    var answer = 0;
+    let answer = 0;
     
-    let sum = n.toString(2).split('').filter(x => x === '1').length;
+    // 1의 개수 찾는 함수
+    const sumFun = numb => numb.toString(2).split('').filter(x => x === '1').length;
     
+    let sum = sumFun(n);
     let sum2 = 0;
     
     while (sum !== sum2) {
         n += 1;
-        sum2 = n.toString(2).split('').filter(x => x === '1').length;
+        sum2 = sumFun(n);
+        
         if (sum === sum2) {
             answer = n;
             break;
