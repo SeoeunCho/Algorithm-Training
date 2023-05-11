1993af1e3ccf7f0ce003e5046e55879b7769c61b
Seoeun, Cho <westsilver24@naver.com>
2023-05-11 16:15:19 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.35 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,25 +1,23 @@
 function solution(survey, choices) {
     let answer = '';
-    const obj = {
-        'R': 0, 
-        'T': 0, 
-        'C': 0, 
-        'F': 0, 
-        'J': 0, 
-        'M': 0, 
-        'A': 0, 
-        'N': 0
-    };
     
-    const check = survey.map((el, i) => {
-        if (choices[i] < 4) obj[el[0]] += 4 - choices[i];
-        if (choices[i] > 4) obj[el[1]] += choices[i] - 4;
+    const MBTI = {};
+    const types = ["RT", "CF", "JM", "AN"];
+    
+    // MBTI 타입별 객체 만들기
+    types.forEach(type => {
+        type.split('').forEach(el => MBTI[el] = 0);
     })
     
-    answer += obj['R'] >= obj['T'] ? 'R' : 'T';
-    answer += obj['C'] >= obj['F'] ? 'C' : 'F';
-    answer += obj['J'] >= obj['M'] ? 'J' : 'M';
-    answer += obj['A'] >= obj['N'] ? 'A' : 'N';
+    const check = survey.map((el, i) => {
+        if (choices[i] < 4) MBTI[el[0]] += 4 - choices[i];
+        if (choices[i] > 4) MBTI[el[1]] += choices[i] - 4;
+    })
+    
+    answer += MBTI['R'] >= MBTI['T'] ? 'R' : 'T';
+    answer += MBTI['C'] >= MBTI['F'] ? 'C' : 'F';
+    answer += MBTI['J'] >= MBTI['M'] ? 'J' : 'M';
+    answer += MBTI['A'] >= MBTI['N'] ? 'A' : 'N';
     
     return answer;
 }
