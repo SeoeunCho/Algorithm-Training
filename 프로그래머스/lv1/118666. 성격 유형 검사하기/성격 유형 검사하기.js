0e2f1dc341958597bd4f5aa84467a5f3878b8136
Seoeun, Cho <westsilver24@naver.com>
2023-05-11 16:06:14 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,43 +1,25 @@
 function solution(survey, choices) {
-    var answer = '';
-//     survey	choices	result
-// ["AN", "CF", "MJ", "RT", "NA"]	[5, 3, 2, 7, 5]	"TCMA"
+    let answer = '';
+    const obj = {
+        'R': 0, 
+        'T': 0, 
+        'C': 0, 
+        'F': 0, 
+        'J': 0, 
+        'M': 0, 
+        'A': 0, 
+        'N': 0
+    };
     
-//     n1 c1 m2 t3 a1
-    let stack = [];
-    let arr = [0, 0, 0, 0, 0, 0, 0, 0];
-    for (let i = 0; i < survey.length; i++) {
-        if (choices[i] === 1) stack.push(survey[i][0] + 3);
-        if (choices[i] === 7) stack.push(survey[i][1] + 3);
-        
-        if (choices[i] === 2) stack.push(survey[i][0] + 2);
-        if (choices[i] === 6) stack.push(survey[i][1] + 2);
-        
-        if (choices[i] === 3) stack.push(survey[i][0] + 1);
-        if (choices[i] === 5) stack.push(survey[i][1] + 1);
-    }
-    console.log(stack)
+    const check = survey.map((el, i) => {
+        if (choices[i] < 4) obj[el[0]] += 4 - choices[i];
+        if (choices[i] > 4) obj[el[1]] += choices[i] - 4;
+    })
     
-    for (let j = 0; j < stack.length; j++) {
-        if (stack[j][0] === 'R') arr[0] += Number(stack[j][1]);
-        if (stack[j][0] === 'T') arr[1] += Number(stack[j][1]);
-        
-        if (stack[j][0] === 'C') arr[2] += Number(stack[j][1]);
-        if (stack[j][0] === 'F') arr[3] += Number(stack[j][1]);
-        
-        if (stack[j][0] === 'J') arr[4] += Number(stack[j][1]);
-        if (stack[j][0] === 'M') arr[5] += Number(stack[j][1]);
-        
-        if (stack[j][0] === 'A') arr[6] += Number(stack[j][1]);
-        if (stack[j][0] === 'N') arr[7] += Number(stack[j][1]);
-    }
-    console.log('arr', arr)
+    answer += obj['R'] >= obj['T'] ? 'R' : 'T';
+    answer += obj['C'] >= obj['F'] ? 'C' : 'F';
+    answer += obj['J'] >= obj['M'] ? 'J' : 'M';
+    answer += obj['A'] >= obj['N'] ? 'A' : 'N';
     
-    answer += arr[0] >= arr[1] ? 'R' : 'T';
-    answer += arr[2] >= arr[3] ? 'C' : 'F';
-    answer += arr[4] >= arr[5] ? 'J' : 'M';
-    answer += arr[6] >= arr[7] ? 'A' : 'N';
-    
-    console.log('answer', answer)
     return answer;
 }
