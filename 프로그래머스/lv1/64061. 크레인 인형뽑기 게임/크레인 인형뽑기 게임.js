c32c5dec4c7527be66094725ef78c33824e1b7be
Seoeun, Cho <westsilver24@naver.com>
2023-05-16 03:19:24 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.96 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,33 @@
+function solution(board, moves) {
+    let answer = 0;
+    
+    const stack = [];
+    board.forEach((x, i) => {
+        stack[i] = new Array();
+    })
+    let num = 0;
+    for (let i = 0; i < board.length; i++) {
+        let list = board[i];
+        for (let j = 0; j < board.length; j++) {
+            num = stack[j].length === 0 ? 0 : stack[j].length;
+            if (list[j] !== 0) stack[j][num] = list[j];
+        }
+    }
+    console.log('stack', stack);
+    
+    let arr = [];
+    for (let i = 0; i < moves.length; i++) {
+        let move = moves[i] - 1;
+        let a = stack[move];
+        let item = a.shift();
+
+        if (item && arr[arr.length - 1] !== item) arr.push(item);
+        else if (item && arr[arr.length - 1] === item) {
+            arr.pop();
+            answer += 2;
+        };
+    }
+    // console.log('arr', arr);
+    
+    return answer;
+}
