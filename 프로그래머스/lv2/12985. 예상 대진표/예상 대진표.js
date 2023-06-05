07708540ddf841e1af294eab0e8e107d39e6082a
Seoeun, Cho <westsilver24@naver.com>
2023-06-05 18:36:30 +0900

    [level 2] Title: 예상 대진표, Time: 13.75 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n, a, b) {
+    let answer = 1;
+
+    for (let i = 1; i <= n; i++) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        if (a === b) continue;
+        answer++;
+        console.log(a, b);
+    }
+    
+    return answer;
+}
