c7df6d55a045dc6dcdf5b23e768ec56cfd8f04d2
Seoeun, Cho <westsilver24@naver.com>
2023-06-07 21:35:58 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,28 +1,25 @@
 function solution(progresses, speeds) {
     let answer = [];
     
-    // 1. 각 며칠이 걸리는지 찾기
+    // 1. 각 작업일 구하기
     const finishDays = progresses.map((el, i) => Math.ceil((100 - el) / speeds[i]));
     
-    
-    // 2. 이전 기능보다 다음 기능이 크면 이전번호+1을 뿌리기, 작으면 기다리고 현재번호+1뿌리기
     let count = 1;
     let len = finishDays.length;
     let prevDay = finishDays[0];
     
     for (let i = 1; i < finishDays.length; i++) {
+        // 2. prevDay가 다음 작업일보다 크거나 같으면 count++, 아직 뿌리지 않음
         if (prevDay >= finishDays[i]) {
             count++;
-            console.log('1===', count);
         } else {
-            answer.push(count);
-            count = 1;
-            prevDay = finishDays[i];
-            console.log('2===', count);
+            answer.push(count);      // 3-1. prevDay가 다음 작업일보다 작아지면 구해진 count뿌리기
+            count = 1;               // 3-2. count 초기화
+            prevDay = finishDays[i]; // 3-3. prevDay에 다음 작업일 대입
         }
     }
     
-    // 마지막으로 처리되지 않은 남은 기능들 푸시
+    // 마지막으로 뿌려지지 않은 남은 기능들의 개수를 구한 count뿌리기
     answer.push(count);
 
     return answer;
