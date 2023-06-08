48515adbf1005187dfcef323b9693e030d17146e
Seoeun, Cho <westsilver24@naver.com>
2023-06-08 16:53:58 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,24 @@
+// H-index는 h 개의 논문이 각각 '적어도 h 개의 인용'을 갖도록 하는 가장 큰 숫자 h 입니다.
+
+function solution(citations) {
+    let len = citations.length;
+    let result = 0;
+    
+    for (let i = 1; i <= len; i++) {
+        let sum = 0;
+        for (let j = 0; j < len; j++) {
+            if (citations[j] >= i) sum++;
+        }
+        // console.log(i, sum);
+        
+        if (sum >= i) result = i;
+    }
+    
+    return result;
+}
+// [6, 5, 3, 1, 0]
+// 1 4
+// 2 3
+// 3 3
+// 4 2
+// 5 2
