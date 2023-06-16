dd68fa2f163569161b219b6209bcaa416b60313f
Seoeun, Cho <westsilver24@naver.com>
2023-06-16 17:26:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,24 +1,23 @@
 function solution(operations) {
     let answer = [0, 0];
-    let arr = [];
+    let queueArr = [];
     
     for (let i = 0; i < operations.length; i++) {
         let num = Number(operations[i].split(' ')[1]);
         if (operations[i][0] === 'I') {
-            arr.push(num);
-            arr.sort((a, b) => a - b);
+            queueArr.push(num);
+            queueArr.sort((a, b) => a - b);
         } else {
-            if (num === -1 && arr.length !== 0) {
-                let findMinNum = Math.min(...arr);
-                arr.shift();
-            } else if (num === 1 && arr.length !== 0) {
-                // arr의 최댓값 삭제
-                let findMaxNum = Math.max(...arr);
-                arr.pop();
+            if (num === -1 && queueArr.length !== 0) {
+                let findMinNum = Math.min(...queueArr);
+                queueArr.shift();
+            } else if (num === 1 && queueArr.length !== 0) {
+                let findMaxNum = Math.max(...queueArr);
+                queueArr.pop();
             }
         }
-        answer = [Math.max(...arr), Math.min(...arr)];
+        answer = [Math.max(...queueArr), Math.min(...queueArr)];
     }
     
-    return arr.length === 0 ? [0, 0] : answer;
+    return queueArr.length === 0 ? [0, 0] : answer;
 }
