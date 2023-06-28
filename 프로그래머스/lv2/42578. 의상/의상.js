f36491aafe9dbae636c1e58f227e9d9747eb821f
Seoeun, Cho <westsilver24@naver.com>
2023-06-28 13:20:58 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,17 +1,21 @@
+// 수학식 풀이 = (의상의 개수 + 1) * (의상의 개수 + 1) - 1
+
 function solution(clothes) {
     let answer = 1;
     
-    let closet = {}; // 저장할 공간
-    for (let [name, kind] of clothes) { // 종류로 옷 분류
-        closet[kind] ? closet[kind].push(name) : closet[kind] = [name]; 
+    const obj = {}; // 저장할 공간
+    
+    // 종류별로 옷 분류
+    // for (let [name, kind] of clothes) {
+    //     obj[kind] ? obj[kind].push(name) : obj[kind] = [name];
+    // }
+    
+    for (let arr of clothes) {
+        obj[arr[1]] = (obj[arr[1]] || 0) + 1;
     }
     
-    clothes = Object.values(closet);
-    if (clothes.length === 1) return clothes[0].length;
-    
-    for (let i = 0; i < clothes.length; i++) {
-        let sum = clothes[i].length;
-        answer *= sum + 1;
+    for (let key in obj) {
+        answer *= (obj[key] + 1);
     }
     
     return answer - 1;
