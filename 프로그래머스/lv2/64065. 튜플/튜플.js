a617061fabbe8a9f3972ace150e5f5b91e7b424f
Seoeun, Cho <westsilver24@naver.com>
2023-06-29 18:22:12 +0900

    [level 2] Title: 튜플, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,28 @@
+function solution(s) {
+    let answer = [];
+    let arr = [];
+    
+    // 1. 원소 배열에 담아줌
+    s.slice(2, s.length-2).split('},{').forEach(el => {
+        arr.push(el.split(',').map(v => Number(v)))
+    })
+    
+    // 2. 원소길이 오름차순 정렬
+    arr.sort((a, b) => a.length - b.length);
+    // 길이가 하나일 경우는 원소 반환
+    if (arr.length === 1) return arr[0];
+    
+    // 3. 길이가 1이상일 경우 튜플구하기
+    let firstEle = arr[0][0];
+    answer.push(firstEle);
+    
+    for (let i = 1; i < arr.length; i++) {
+        // answer에 있는 원소와 다르면 answer에 push
+        for (let j = 0; j < arr[i].length; j++) {
+            if (!answer.includes(arr[i][j])) answer.push(arr[i][j]);
+        }
+    }
+    
+    
+    return answer;
+}
