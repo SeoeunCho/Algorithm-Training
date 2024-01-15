5a252e647f768ae48053b0c0c38f12cf4ab2c755
조서은 <westsilver24@naver.com>
2024-01-15 21:24:52 +0900

    [level Lv.0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ');
+    const operator = {
+        '+' : (a, b) => a + b,
+        '-' : (a, b) => a - b,
+        '*' : (a, b) => a * b
+    }
+    
+    return operator[op](Number(a), Number(b));
+}
