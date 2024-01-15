044e19ebf528361cc80f7177812854e7ad039277
조서은 <westsilver24@naver.com>
2024-01-15 21:35:56 +0900

    [level Lv.0] Title: 수 조작하기 2, Time: 12.17 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181925. 수 조작하기 2/README.md
+++ 프로그래머스/Lv.0/181925. 수 조작하기 2/README.md
@@ -1,10 +1,10 @@
 # [level Lv.0] 수 조작하기 2 - 181925 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181925) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181925?language=javascript) 
 
 ### 성능 요약
 
-메모리: 41.6 MB, 시간: 8.81 ms
+메모리: 40.9 MB, 시간: 12.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 21:33:44
+2024년 1월 1일 21:35:54
 
 ### 문제 설명
 
--- 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,6 +1,7 @@
 function solution(numLog) {
     let answer = '';
     
+    /* 초기풀이
     for (let i = 1; i < numLog.length; i++) {
         let calc = numLog[i] - numLog[i - 1];
         if (calc === 1) {
@@ -15,4 +16,16 @@ function solution(numLog) {
     }
     
     return answer;
+    */
+    
+    const convert = {
+        '1': 'w', 
+        '-1': 's', 
+        '10': 'd', 
+        '-10': 'a'
+    };
+
+    return numLog.slice(1).map((v, i) => {
+        return convert[v - numLog[i]]
+    }).join('');
 }
