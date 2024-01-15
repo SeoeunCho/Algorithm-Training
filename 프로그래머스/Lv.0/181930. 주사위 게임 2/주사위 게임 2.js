ffd6f1343d9a01d30ce3a95e19378375f79685dc
조서은 <westsilver24@naver.com>
2024-01-15 22:43:17 +0900

    [level Lv.0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,17 +1,13 @@
 function solution(a, b, c) {
     const sum = a + b + c;
+    const sum2 = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+    const sum3 = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
     
-    if (a !== b && b !== c && c !== a) {
-        return sum;
+    if (a === b && b === c) {
+        return sum * sum2 * sum3;
+    } else if (a === b || b === c || c === a) {
+        return sum * sum2;
     } else {
-        const pow2 = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
-        
-        if (a == b && b == c && c == a) {
-            const pow3 = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
-            return sum * pow2 * pow3;
-        } else {
-            return sum * pow2;
-        }
-        
+        return sum;
     }
 }
