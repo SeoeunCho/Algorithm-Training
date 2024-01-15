27ab9a73c52b402cbb7f7072da1db3786a83422f
조서은 <westsilver24@naver.com>
2024-01-15 19:54:32 +0900

    [level Lv.0] Title: 0 떼기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/Lv.0/181847. 0 떼기/0 떼기.js
@@ -1,7 +1,11 @@
-function solution(n_str) {    
+function solution(n_str) {
+    /* 초기 풀이
     for (let i = 0; i < n_str.length; i++) {
         if (n_str[i] !== '0') {
             return n_str.slice(i);
         }
     }
+    */
+    
+    return n_str.replace(/^0+/, '');
 }
