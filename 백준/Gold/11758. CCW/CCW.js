79c8cb0f9bfa717bc3da9f53331cf2466cb01acc
조서은 <westsilver24@naver.com>
2024-01-29 13:57:21 +0900

    [Gold V] Title: CCW, Time: 152 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.js
+++ 백준/Gold/11758. CCW/CCW.js
@@ -1,12 +1,16 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n').map(el => el.split(' ').map(Number));
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let [x1, y1] = input[0];
-let [x2, y2] = input[1];
-let [x3, y3] = input[2];
+// 각 좌표값을 숫자로 변환
+let points = input.map(el => el.split(' ').map(Number));
+
+let [x1, y1] = points[0];
+let [x2, y2] = points[1];
+let [x3, y3] = points[2];
+
+// Counter Clockwise 알고리즘
 let ccw = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3);
 
-// ccw > 0 반시계 방향, ccw = 0 일직선, ccw < 0 시계방향
 if (ccw > 0) {
     console.log(1);
 } else if (ccw === 0) {
