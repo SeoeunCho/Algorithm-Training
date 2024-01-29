28a012a72eb7cac02a8376e379c4e1face622dee
조서은 <westsilver24@naver.com>
2024-01-29 14:57:09 +0900

    [Bronze II] Title: 상수, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,11 @@
+// fs 모듈을 이용해 파일 전체를 읽어와 문자열로 저장하기
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// 문자열을 수로 변환할 때 parseInt에 비하여 Number의 속도가 더 빠르게 동작
+let [a, b] = input[0].split(' ');
+
+let newA = a[2] + a[1] + a[0];
+let newB = b[2] + b[1] + b[0];
+
+console.log(Math.max(Number(newA), Number(newB)));
