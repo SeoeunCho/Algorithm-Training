109245bc843914d7c25820b165c82db026f6de7f
조서은 <westsilver24@naver.com>
2024-01-29 13:25:52 +0900

    [Gold V] Title: CCW, Time: 164 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [x1, y1] = input[0].split(' ');
+let [x2, y2] = input[1].split(' ');
+let [x3, y3] = input[2].split(' ');
+let ccw = (x1*y2+x2*y3+x3*y1) - (x2*y1+x3*y2+x1*y3);
+
+// ccw > 0 반시계 방향, ccw = 0 일직선, ccw < 0 시계방향
+if (ccw > 0) {
+    console.log(1);
+} else if (ccw === 0) {
+    console.log(0);
+} else {
+    console.log(-1);
+}
